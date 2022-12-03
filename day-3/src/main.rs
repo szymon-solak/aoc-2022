ac1d4d46569024ad2fc93fcae1af6a57d15fd97e
szymon-solak <16944591+szymon-solak@users.noreply.github.com>
2022-12-03 13:33:35 +0100

    feat: solve day 3 part 2

--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -1,24 +1,32 @@
+fn to_priority(ch: char) -> u64 {
+    if ch.is_ascii_lowercase() {
+        (ch as u8 - 96).into()
+    } else {
+        (ch as u8 - 38).into()
+    }
+}
+
 fn main() {
     let input = std::fs::read_to_string("input.txt").unwrap();
 
-    let priorities = input
+    let rucksacks: Vec<&str> = input
         .lines()
-        .map(|l| {
-            let (l, r) = l.split_at(l.len() / 2);
+        .collect();
 
-            for ch in l.chars() {
-                if r.contains(ch) { return ch; }
+    let groups = rucksacks
+        .chunks(3)
+        .map(|elf_group| {
+            let elf_a = elf_group.get(0).unwrap();
+            let elf_b = elf_group.get(1).unwrap();
+            let elf_c = elf_group.get(2).unwrap();
+
+            for ch in elf_a.chars() {
+                if elf_b.contains(ch) && elf_c.contains(ch) { return ch }
             }
 
             unreachable!()
         })
-        .map(|ch| {
-            if ch.is_ascii_lowercase() {
-                ((ch as u8) - 96) as u64
-            } else {
-                ((ch as u8) - 38) as u64
-            }
-        });
+        .map(|ch| to_priority(ch));
 
-    println!("sum of priorities: {}", priorities.sum::<u64>())
+    println!("sum of priorities: {}", groups.sum::<u64>())
 }
