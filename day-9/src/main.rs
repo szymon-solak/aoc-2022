01ab6cbd0e4d834fd85e7ec0123bb98e3fa241c8
Szymon Solak <16944591+szymon-solak@users.noreply.github.com>
2022-12-10 17:56:35 +0100

    feat: solve day 9 part 2

--- day-9/src/main.rs
+++ day-9/src/main.rs
@@ -2,12 +2,6 @@ use std::{collections::HashSet, convert::Infallible, num::ParseIntError, str::Fr
 
 type Position = (i64, i64);
 
-#[derive(Default, Debug)]
-struct Rope {
-    head: Position,
-    tail: Position,
-}
-
 #[derive(Debug)]
 enum Direction {
     Up,
@@ -60,46 +54,89 @@ impl Movement {
     }
 }
 
-impl Rope {
-    fn apply_movement(self, movement: &Movement) -> (Rope, Vec<Position>) {
-        dbg!(self.head, self.tail, movement);
+struct MultiKnotRope {
+    knots: Vec<Position>,
+}
 
+impl MultiKnotRope {
+    fn adjust_tail(head: Position, tail: Position) -> Position {
+        let head_tail_distance = (head.0 - tail.0, head.1 - tail.1);
+
+        let tail_delta = match head_tail_distance {
+            (0, 2) => (0, 1),
+            (0, -2) => (0, -1),
+            (2, 0) => (1, 0),
+            (-2, 0) => (-1, 0),
+            (2, 1) => (1, 1),
+            (2, -1) => (1, -1),
+            (-2, 1) => (-1, 1),
+            (-2, -1) => (-1, -1),
+            (1, 2) => (1, 1),
+            (1, -2) => (1, -1),
+            (-1, 2) => (-1, 1),
+            (-1, -2) => (-1, -1),
+            (-2, -2) => (-1, -1),
+            (2, 2) => (1, 1),
+            (-2, 2) => (-1, 1),
+            (2, -2) => (1, -1),
+            _ => (0, 0),
+        };
+
+        (tail.0 + tail_delta.0, tail.1 + tail_delta.1)
+    }
+
+    fn apply_movement(self, movement: &Movement) -> (MultiKnotRope, Vec<Position>) {
         let mut visited: Vec<Position> = vec![];
-        let rope_after_move = movement.into_steps().iter().inspect(|step| { dbg!(step); }).fold(self, |rope, head_delta| {
-            let head = (rope.head.0 + head_delta.0, rope.head.1 + head_delta.1);
-            let head_tail_distance = (head.0 - rope.tail.0, head.1 - rope.tail.1);
 
-            let tail_delta = match head_tail_distance {
-                (0, 2) => (0, 1),
-                (0, -2) => (0, -1),
-                (2, 0) => (1, 0),
-                (-2, 0) => (-1, 0),
-                (2, 1) => (1, 1),
-                (2, -1) => (1, -1),
-                (-2, 1) => (-1, 1),
-                (-2, -1) => (-1, -1),
-                (1, 2) => (1, 1),
-                (1, -2) => (1, -1),
-                (-1, 2) => (-1, 1),
-                (-1, -2) => (-1, -1),
-                _ => (0, 0),
-            };
+        let rope_after_move = movement
+            .into_steps()
+            .iter()
+            .fold(self, |rope, step| {
+                let head = rope.knots.get(0).unwrap();
+                let mut knots: Vec<Position> = vec![
+                    (head.0 + step.0, head.1 + step.1)
+                ];
 
-            dbg!(head, tail_delta);
+                for knot in rope.knots.iter().skip(1) {
+                   knots.push(
+                        MultiKnotRope::adjust_tail(
+                            *knots.last().unwrap(),
+                            *knot,
+                        )
+                    ) 
+                }
 
-            let tail = (rope.tail.0 + tail_delta.0, rope.tail.1 + tail_delta.1);
+                visited.push(*knots.last().unwrap());
 
-            visited.push(tail);
-
-            Rope { head, tail }
-        });
+                MultiKnotRope { knots }
+            });
 
         (rope_after_move, visited)
     }
 }
 
+fn print_rope(rope: &MultiKnotRope) {
+    print!("{}[2J", 27 as char); // clear
+    
+    for y in (-64..64).rev() {
+        let mut line: String = "".to_string();
+
+        for x in -64..64 {
+            if let Some(_) = rope.knots.iter().find(|k| k.0 == x && k.1 == y) {
+                line.push_str("#");
+            } else {
+                line.push_str(".");
+            }
+        }
+
+        println!("{line}");
+    } 
+}
+
 fn main() {
-    let rope: Rope = Default::default();
+    let rope = MultiKnotRope {
+        knots: vec![(0, 0)].repeat(10),
+    };
     let moves: Vec<Movement> = include_str!("../input.txt")
         .lines()
         .flat_map(|line| line.parse::<Movement>())
@@ -109,11 +146,15 @@ fn main() {
 
     let _final_rope_position = moves.iter().fold(rope, |rope, movement| {
         let (rope_after_move, visited_in_move) = rope.apply_movement(movement);
-        
-        for m in visited_in_move { tail_visited_fields.insert(m); }
+
+        // print_rope(&rope_after_move);
+
+        for m in visited_in_move {
+            tail_visited_fields.insert(m);
+        }
 
         rope_after_move
     });
-
-    println!("part 1: {}", tail_visited_fields.len())
+    
+    println!("part 2: {:?}", tail_visited_fields.len());
 }
